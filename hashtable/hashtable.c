450b85bf9465d5e442953bb1f7378c25b72f1fa8
Matyáš Strelec <matyasstrelec@gmail.com>
2022-10-27 17:00:05 +0200

    test on eva, add comments

--- hashtable/hashtable.c
+++ hashtable/hashtable.c
@@ -35,11 +35,7 @@ int get_hash(char *key)
  */
 void ht_init(ht_table_t *table)
 {
-	if (table == NULL)
-	{
-		return;
-	}
-
+	// Initialize each element of table to null
 	for (int i = 0; i < HT_SIZE; i++)
 	{
 		(*table)[i] = NULL;
@@ -54,6 +50,7 @@ void ht_init(ht_table_t *table)
  */
 ht_item_t *ht_search(ht_table_t *table, char *key)
 {
+	// Return NULL if table is empty or key doesn't exist
 	if (table == NULL || key == NULL)
 	{
 		return NULL;
@@ -61,17 +58,22 @@ ht_item_t *ht_search(ht_table_t *table, char *key)
 
 	int hash = get_hash(key);
 
+	// Find item based on hash
 	ht_item_t *item = (*table)[hash];
 
+	// Go through all items in the table on the same hash
 	while (item != NULL)
 	{
+		// If found, return the item
 		if (strcmp(item->key, key) == 0)
 		{
 			return item;
 		}
+		// Move onto another
 		item = item->next;
 	}
 
+	// If not found, return NULL
 	return NULL;
 }
 
@@ -85,6 +87,7 @@ ht_item_t *ht_search(ht_table_t *table, char *key)
  */
 void ht_insert(ht_table_t *table, char *key, float value)
 {
+	// Return if table is empty or key doesn't exist
 	if (table == NULL || key == NULL)
 	{
 		return;
@@ -92,6 +95,7 @@ void ht_insert(ht_table_t *table, char *key, float value)
 
 	ht_item_t *item = ht_search(table, key);
 
+	// If item exists, replace its value
 	if (item != NULL)
 	{
 		item->value = value;
@@ -100,16 +104,21 @@ void ht_insert(ht_table_t *table, char *key, float value)
 
 	int hash = get_hash(key);
 
+	// Create new item
 	ht_item_t *newItem = malloc(sizeof(ht_item_t));
 	
+	// Malloc fail
 	if (newItem == NULL)
 	{
 		return;
 	}
 
+	// Set values
 	newItem->key = key;
 	newItem->value = value;
 	newItem->next = (*table)[hash];
+
+	// Add new item to table
 	(*table)[hash] = newItem;
 }
 
@@ -123,6 +132,7 @@ void ht_insert(ht_table_t *table, char *key, float value)
  */
 float *ht_get(ht_table_t *table, char *key)
 {
+	// Return NULL if table is empty or key doesn't exist
 	if (table == NULL || key == NULL)
 	{
 		return NULL;
@@ -130,6 +140,7 @@ float *ht_get(ht_table_t *table, char *key)
 
 	ht_item_t *item = ht_search(table, key);
 
+	// If item exists, return its value
 	if (item != NULL)
 	{
 		return &item->value;
@@ -148,6 +159,7 @@ float *ht_get(ht_table_t *table, char *key)
  */
 void ht_delete(ht_table_t *table, char *key)
 {
+	// Return if table is empty or key doesn't exist
 	if (table == NULL || key == NULL)
 	{
 		return;
@@ -155,24 +167,33 @@ void ht_delete(ht_table_t *table, char *key)
 
 	int hash = get_hash(key);
 
+	// Find item based on hash
 	ht_item_t *item = (*table)[hash];
 	ht_item_t *prev = NULL;
 
+	// Go through all items in the table on the same hash
 	while (item != NULL)
 	{
+		// If found, delete it
 		if (strcmp(item->key, key) == 0)
 		{
+			// If it's the first item in the list
 			if (prev == NULL)
-			{
+			{	
+				// Set the next to be first
 				(*table)[hash] = item->next;
 			}
+			// If it's not the first
 			else
 			{
+				// Step over the deleted item
 				prev->next = item->next;
 			}
 			free(item);
 			return;
 		}
+
+		// Move onto another
 		prev = item;
 		item = item->next;
 	}
@@ -186,6 +207,7 @@ void ht_delete(ht_table_t *table, char *key)
  */
 void ht_delete_all(ht_table_t *table)
 {
+	// Return if table is empty
 	if (table == NULL)
 	{
 		return;
@@ -194,15 +216,19 @@ void ht_delete_all(ht_table_t *table)
 	ht_item_t *item;
 	ht_item_t *next;
 
+	// Go through all items in the table
 	for (int i = 0; i < HT_SIZE; i++)
 	{
 		item = (*table)[i];
+		
+		// Go through all items in the list
 		while (item != NULL)
 		{
 			next = item->next;
 			free(item);
 			item = next;
 		}
+		// Set the list to NULL
 		(*table)[i] = NULL;
 	}
 }
